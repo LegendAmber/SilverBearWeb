e434f105f04099e55054e8865cd869a7139bec59
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-10-26 12:52:37 -0700

    replit

--- Pages/Sources/Script/web-assets.js
+++ Pages/Sources/Script/web-assets.js
@@ -3,9 +3,6 @@ screen.style.cursor = "none";
 screen.width = window.innerWidth;
 screen.height = window.innerHeight;
 screen.style.backgroundColor = "black";
-
-var foot = document.querySelector('footer');
-foot.style.objectPosition = window.innerWidth;
 function screenTotal(){
   
 }
