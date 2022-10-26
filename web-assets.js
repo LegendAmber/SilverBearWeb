69182ac650b11c28a8e7c860d384911f45b8c97f
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-10-26 12:15:31 -0700

    Create web-assets.js

---
+++ web-assets.js
@@ -0,0 +1,9 @@
+var screen = document.querySelector('body');
+screen.style.cursor = "none";
+screen.width = window.innerWidth;
+screen.height = window.innerHeight;
+screen.style.backgroundColor = "black";
+
+function screenTotal(){
+  
+}
